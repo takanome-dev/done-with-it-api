1e353aa8715088a48e9ab3d81801ca7fc942a1a4
TAKANOME-DEV <79809121+TAKANOME-DEV@users.noreply.github.com>
2022-04-11 01:45:54 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const compression = require("compression");
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+const auth = require("./routes/auth");
+const categories = require("./routes/categories");
+const expoPushTokens = require("./routes/expoPushTokens");
+const listings = require("./routes/listings");
+const messages = require("./routes/messages");
+const users = require("./routes/users");
+const userListings = require("./routes/userListings");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use("/api/auth", auth);
+app.use("/api/categories", categories);
+app.use("/api/expoPushTokens", expoPushTokens);
+app.use("/api/listings", listings);
+app.use("/api/messages", messages);
+app.use("/api/users", users);
+app.use("/api/userListings", userListings);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
---
+++ middleware/validation.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+const store = require("../store/categories");
+
+const validateSchema = (schema) => (req, res, next) => {
+	const result = Joi.object(schema).validate(req.body);
+
+	if (result.error)
+		return res.status(400).send({ error: result.error.details[0].message });
+
+	return next();
+};
+
+const validateCategoryId = async (req, res, next) => {
+	const category = await store.getCategory(parseInt(req.body.categoryId));
+
+	if (!category) return res.status(400).send({ error: "Invalid categoryId." });
+
+	next();
+};
+
+module.exports = {
+	validateSchema,
+	validateCategoryId,
+};
